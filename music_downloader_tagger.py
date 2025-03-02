d6cc8b727e9ce37789fdec3b241e183087abf21d
Stergious Aji <2546916A@student.gla.ac.uk>
2025-03-02 16:24:42 +0000

    Added yt-dlp as backup option

--- music_downloader_tagger.py
+++ music_downloader_tagger.py
@@ -1,5 +1,5 @@
-import pytubefix as pytube
 # import pytube
+import pytubefix as pytube
 import subprocess
 import os, shutil, argparse
 from ShazamAPI import Shazam
@@ -7,12 +7,11 @@ import urllib
 import music_tag
 import requests
 import subprocess
-
-import youtube_dl
+import yt_dlp
 
 from ytm_automator import scrape_urls, download_mp3
-
 from spotify_yt_scraper import find_playlist, scrape_song_info, search_ytmusic, manual_search, RESET, GREEN, YELLOW, RED, MAGENTA, BLUE
+
 
 def clean_folder(filename):
     if "downloads" in os.listdir():
@@ -37,20 +36,21 @@ def download_audio(url):
 
     audio_file = f"{out_file.split('.')[0]}.mp3"
     print(f"Downloading {MAGENTA}{os.path.basename(audio_file)}{RESET}")
-    subprocess.run(['ffmpeg', '-i', out_file, '-b:a', '192K', audio_file, '-hide_banner', '-loglevel', 'quiet'])
+    subprocess.run(["ffmpeg", '-i', out_file, "-b:a", "192K", audio_file, "-hide_banner", "-loglevel", "quiet"])
     os.remove(out_file)
     return audio_file
 
 def download_audio_YDL(url):
     ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
+        "format": "bestaudio/best",
+        "postprocessors": [{
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "192",
         }],
+        "outtmpl": "downloads/%(title)s.%(ext)s"
     }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
 def recognise_song(file_path):
@@ -63,10 +63,10 @@ def recognise_song(file_path):
             return
 
         recognised = recognised["track"]
-        metadata['title'] = urllib.parse.unquote_plus(recognised["urlparams"]["{tracktitle}"])
-        metadata['artist'] = urllib.parse.unquote_plus(recognised["urlparams"]["{trackartist}"])
+        metadata["title"] = urllib.parse.unquote_plus(recognised["urlparams"]["{tracktitle}"])
+        metadata["artist"] = urllib.parse.unquote_plus(recognised["urlparams"]["{trackartist}"])
         if "images" in recognised:
-            metadata['imageURL'] = recognised["images"]["coverart"]
+            metadata["imageURL"] = recognised["images"]["coverart"]
     
     return metadata
 
@@ -78,17 +78,17 @@ def validate_filename(filename):
     return filename
 
 def modify_file(file_path, metadata):
-    new_filename = validate_filename(f"{metadata['artist']} - {metadata['title']}")
+    new_filename = validate_filename(f"{metadata["artist"]} - {metadata["title"]}")
     new_filename = os.path.join("downloads", new_filename)
 
     if not os.path.exists(new_filename):
         os.rename(file_path, f"{new_filename}.mp3")
         song = music_tag.load_file(f"{new_filename}.mp3")
-        song['tracktitle'] = metadata['title']
-        song['artist'] = metadata['artist']
-        song['albumartist'] = metadata['artist']
+        song["tracktitle"] = metadata["title"]
+        song["artist"] = metadata["artist"]
+        song["albumartist"] = metadata["artist"]
         if "imageURL" in metadata:
-            song['artwork'] = requests.get(metadata['imageURL']).content
+            song["artwork"] = requests.get(metadata["imageURL"]).content
 
         song.save()
     else:
@@ -98,8 +98,8 @@ def modify_file(file_path, metadata):
 parser = argparse.ArgumentParser(description="Program to download and tag mp3 audio files.")
 parser.add_argument("-c", "--clean", help="Option to clean downloads folder and passed urls file", nargs='?', type=str, const=" ")
 parser.add_argument("-f", "--file", help="Pass in text file of URLs (defaults to urls.txt)", nargs='?', type=str, const="urls.txt")
-parser.add_argument("-y", "--ytm", help="Use YouTubeToMP3 to download songs", action="store_true")
-parser.add_argument("-s", "--spotify", help="Scrape songs from Spotify", nargs='?', type=str, const="new-songs")
+parser.add_argument("-y", "--ytdlp", help="Use YT-DLP to download songs", action="store_true")
+parser.add_argument("-s", "--spotify", help="Scrape songs from a Spotify playlist", nargs='?', type=str, const="new-songs")
 parser.add_argument("-m", "--manual_search", help="Manually search terms on YouTube Music", action="store_true")
 
 args = parser.parse_args()
@@ -121,8 +121,8 @@ if __name__ == "__main__":
                     url_list.append(url)
     
     if (args.spotify):
-        pl_new_songs = find_playlist(args.spotify)
-        songs = scrape_song_info(pl_new_songs)
+        playist = find_playlist(args.spotify)
+        songs = scrape_song_info(playist)
         print()
         url_list.extend(search_ytmusic(songs))
     elif (args.manual_search):
@@ -139,12 +139,17 @@ if __name__ == "__main__":
         
     # Download and modify metadata
     print()
-    if (args.ytm):
-        scrape_urls(url_list)
-        download_mp3()
+    # [DEPRECATED] YoutubeToMP3 terminated
+    # if (args.ytm):
+    #     scrape_urls(url_list)
+    #     download_mp3()
+    if (args.ytdlp):
+        download = download_audio_YDL
     else:
-        for url in url_list:
-            download_audio(url)
+        download = download_audio
+    
+    for url in url_list:
+        download(url)
 
     print()
     # Modify unmodified downloads in order of modified time
@@ -154,7 +159,7 @@ if __name__ == "__main__":
         if not song["tracktitle"]:
             metadata = recognise_song(audio_file_path)
             if metadata:
-                print(f"Modifying {BLUE}{metadata['artist']} - {metadata['title']}{RESET}")
+                print(f"Modifying {BLUE}{metadata["artist"]} - {metadata["title"]}{RESET}")
                 modify_file(audio_file_path, metadata)
             else:
                 print(f"Skipping {RED}{os.path.basename(audio_file_path)}{RESET}")
